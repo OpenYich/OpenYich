dbbdfb6d88121f86506d6e9ab6e3a5ac35bf269e
zhycn <qzhservice@gmail.com>
2018-11-08 14:07:17 +0800

    Initial commit

--- openyich-framework/src/main/java/com/openyich/framework/autoconfigure/OpenyichAutoConfiguration.java
+++ openyich-framework/src/main/java/com/openyich/framework/autoconfigure/OpenyichAutoConfiguration.java
@@ -3,6 +3,12 @@ package com.openyich.framework.autoconfigure;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * OpenYich Auto-configure
+ * 
+ * @author zhycn
+ * @since 2.1.0 2018-11-08
+ */
 @Configuration
 @ComponentScan("com.openyich.framework")
 public class OpenyichAutoConfiguration {
